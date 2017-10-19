5d335692a5ac8a0b6bc9ae0ece7cdbc865d353b6
李良明 <294408485@qq.com>
2017-10-19 14:17:58 +0800

    new file:   XMMoneyTextField/XMMoneyTextField.xcodeproj/project.xcworkspace/xcuserdata/liliangming.xcuserdatad/UserInterfaceState.xcuserstate 	new file:   XMMoneyTextField/XMMoneyTextField.xcodeproj/xcuserdata/liliangming.xcuserdatad/xcschemes/xcschememanagement.plist 	new file:   XMMoneyTextField/XMMoneyTextField/AppDelegate.h 	new file:   XMMoneyTextField/XMMoneyTextField/AppDelegate.m 	new file:   XMMoneyTextField/XMMoneyTextField/Assets.xcassets/AppIcon.appiconset/Contents.json 	new file:   XMMoneyTextField/XMMoneyTextField/BXTextField.h 	new file:   XMMoneyTextField/XMMoneyTextField/BXTextField.m 	new file:   XMMoneyTextField/XMMoneyTextField/Base.lproj/LaunchScreen.storyboard 	new file:   XMMoneyTextField/XMMoneyTextField/Base.lproj/Main.storyboard 	new file:   XMMoneyTextField/XMMoneyTextField/Info.plist 	new file:   XMMoneyTextField/XMMoneyTextField/ViewController.h 	new file:   XMMoneyTextField/XMMoneyTextField/ViewController.m 	new file:   XMMoneyTextField/XMMoneyTextField/main.m 	new file:   XMMoneyTextField/XMMoneyTextFieldTests/Info.plist 	new file:   XMMoneyTextField/XMMoneyTextFieldTests/XMMoneyTextFieldTests.m 	new file:   XMMoneyTextField/XMMoneyTextFieldUITests/Info.plist 	new file:   XMMoneyTextField/XMMoneyTextFieldUITests/XMMoneyTextFieldUITests.m
    我的输入框

---
+++ XMMoneyTextField/XMMoneyTextField/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMMoneyTextField
+//
+//  Created by 李良明 on 2017/10/19.
+//  Copyright © 2017年 李良明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
